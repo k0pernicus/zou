420e7dc71ae80619b1ae820fe3e035300b153b54
k0pernicus <antonin.carette@gmail.com>
2017-06-13 13:31:09 +0200

    Add new function to know if the response from a server is ok or not

--- src/response.rs
+++ src/response.rs
@@ -5,10 +5,14 @@ pub trait CheckResponseStatus {
     /// Function to check if the `PartialContent` status is contained
     /// in the HTTP header response
     fn check_partialcontent_status(&self) -> bool;
+    fn is_ok(&self) -> bool;
 }
 
 impl CheckResponseStatus for Response {
     fn check_partialcontent_status(&self) -> bool {
         self.status == StatusCode::PartialContent
+    }
+    fn is_ok(&self) -> bool {
+        self.status.is_success()
     }
 }
