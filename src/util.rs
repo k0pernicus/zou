c80cf5cd2fd73f64315388002e9829ee5972410a
k0pernicus <antonin.carette@gmail.com>
2017-10-02 08:37:42 +0200

    Add developer documentation

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,7 @@
 use std::io;
 use std::io::Write;
 
+/// Deliver a way to interact with the user, using the keyboard
 pub fn prompt_user(prompt: &str) -> String {
     warning!(prompt);
     io::stdout().flush().expect("Couldn't flush stdout!");
@@ -10,5 +11,6 @@ pub fn prompt_user(prompt: &str) -> String {
         .read_line(&mut user_input)
         .ok()
         .expect("Couldn't read line!");
+    // Remove w+
     String::from(user_input.trim())
 }
